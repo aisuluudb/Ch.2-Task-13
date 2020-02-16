e6c8a23c7ff228a1756a35a9e446edbe593a7732
Aisuluu DB <dzhaisuluu@gmail.com>
2020-02-16 21:00:55 +0600

    Add files via upload

---
+++ Task13.py
@@ -0,0 +1,20 @@
+
+
+# n = int (input("Enter the number.."))
+n = 25
+i = 1
+while i**i <= n:
+    print (i**i)
+    i += 1
+    
+
+
+
+
+
+
+#     x = int
+# i = 1
+# while i*i <= x:
+#    print(i*i)
+#    i += 1
